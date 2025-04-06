d33c483e1cc3fbd3804a38ecec051cd3acb46a22
Gokay Iseri <gokay.iseri@outlook.com>
2025-04-06 23:44:29 +0300

    Explosion Particle Added

--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -6,7 +6,11 @@ public class EnemyMovement : MonoBehaviour
     private Player m_player;
 
     [Header("Settings")]
-    [SerializeField] private int m_moveSpeed;
+    [SerializeField] private float m_moveSpeed;
+    [SerializeField] private float m_playerDetectionDistance; // minimum distance to player
+
+    [Header("DEBUG")]
+    [SerializeField] private bool m_isGizmosEnabled;
 
     void Awake()
     {
@@ -21,6 +25,13 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
+        FollowPlayer();
+
+        TryAttack();
+    }
+
+    private void FollowPlayer()
+    {
         Vector2 directionVector = m_player.transform.position - transform.position; // a vector from enemy to player
         Vector2 normalizedDirectionVector = directionVector.normalized; // normalize it to get raw vector
 
@@ -29,4 +40,28 @@ public class EnemyMovement : MonoBehaviour
 
         transform.position = targetPosition; // every frame we assign the new position
     }
+
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, m_player.transform.position); // get the distance to player
+
+        if (distanceToPlayer < m_playerDetectionDistance)
+        {
+            Debug.Log("Enemy is attacking the player!");
+            Destroy(gameObject); // destroy the enemy when it is close to player
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (m_isGizmosEnabled == false)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red; // Set the color of the Gizmos to red
+
+        Gizmos.DrawWireSphere(transform.position, m_playerDetectionDistance); // Draw a wire sphere at the enemy's position with the detection distance
+    }
+
 }
